5d532224f5358dad535107b08a2549ae4a2655a3
Lian <lianythompson@gmail.com>
2025-09-05 21:16:24 -0700

    favicon

--- src/app/jobCard.tsx
+++ src/app/jobCard.tsx
@@ -17,15 +17,15 @@ export default function JobCard({ jobs, link }: JobCardProps) {
     return (
         <a href={link} target="_blank">
             <div className={styles.container}>
-                {jobs.map((job) => (
-                    <div className={styles.job_container}>
+                {jobs.map((job, idx) => (
+                    <div className={styles.job_container} key={idx}>
                         <div className={styles.dates}>{job.start} &mdash; {job.end}</div>
                         <div className={styles.content}>
                             <div className={styles.title_container}>
                                 <div className={styles.title}>{job.title}</div>
                                 <div className={styles.company}>{job.company}</div>
                             </div>
-                            <div className={styles.description}>{job.description}</div>
+                            <div>{job.description}</div>
                         </div>
                     </div>
                 ))}
